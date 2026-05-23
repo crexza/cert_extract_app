3bc0173704fdea967f5cb42937fc1e2979b9309d
crexza <caressa2004@gmail.com>
2026-05-24 05:14:02 +0800

    HD test live frontend automatic deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -408,7 +408,7 @@ const loadCollectionData = async (collectionName) => {
       />
       <div className="w-full text-center py-2 bg-gray-100 dark:bg-gray-800">
         <p className="text-sm text-gray-500 dark:text-gray-400">
-          Deployment Version: v1.1
+          Deployment Version: v1.2 update
         </p>
       </div>
 
